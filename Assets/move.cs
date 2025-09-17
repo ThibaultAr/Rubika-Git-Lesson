14a1e3232b442c25ab0e74b25d9b0f0af2e7cffc
ebail-ui <e.bail@rubika-edu.com>
2025-09-17 15:05:47 +0200

    wow

--- Assets/move.cs
+++ Assets/move.cs
@@ -3,16 +3,13 @@ using UnityEngine;
 public class AutoMove : MonoBehaviour
 {
     public Vector3 direction = Vector3.forward; // Direction du mouvement (par défaut: avant Z+)
-    public float speed = 5f;    
-   
-    // Vitesse en unités/seconde
+    public float speed = 5f;                     // Vitesse en unités/seconde
     private void Start()
     {
-        
+        Debug.Log("jsp");
     }
     void Update()
     {
         transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
     }
-
 }
